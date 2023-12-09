2bc3282b434712e467b06460f8d7980b7054e5c1
Gwilym Inzani <email@gwilym.dev>
2023-12-09 08:33:46 +0000

    Part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let input = include_str!("../input.txt");
     println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
 }
 
 fn part1(input: &str) -> i64 {
@@ -11,12 +12,25 @@ fn part1(input: &str) -> i64 {
                 .split_ascii_whitespace()
                 .map(|number| number.parse().unwrap())
                 .collect::<Vec<_>>();
-            extrapolate(&numbers)
+            extrapolate_end(&numbers)
         })
         .sum::<i64>()
 }
 
-fn extrapolate(input: &[i64]) -> i64 {
+fn part2(input: &str) -> i64 {
+    input
+        .lines()
+        .map(|line| {
+            let numbers = line
+                .split_ascii_whitespace()
+                .map(|number| number.parse().unwrap())
+                .collect::<Vec<_>>();
+            extrapolate_start(&numbers)
+        })
+        .sum::<i64>()
+}
+
+fn extrapolate_end(input: &[i64]) -> i64 {
     if input.iter().all(|&x| x == 0) {
         return 0;
     }
@@ -26,12 +40,25 @@ fn extrapolate(input: &[i64]) -> i64 {
         .map(|window| window[1] - window[0])
         .collect();
 
-    extrapolate(&differences) + input.last().unwrap()
+    extrapolate_end(&differences) + input.last().unwrap()
+}
+
+fn extrapolate_start(input: &[i64]) -> i64 {
+    if input.iter().all(|&x| x == 0) {
+        return 0;
+    }
+
+    let differences: Vec<_> = input
+        .windows(2)
+        .map(|window| window[1] - window[0])
+        .collect();
+
+    input[0] - extrapolate_start(&differences)
 }
 
 #[test]
 fn test_extrapolate() {
-    assert_eq!(extrapolate(&[0, 3, 6, 9, 12, 15]), 18);
-    assert_eq!(extrapolate(&[1, 3, 6, 10, 15, 21]), 28);
-    assert_eq!(extrapolate(&[10, 13, 16, 21, 30, 45]), 68);
+    assert_eq!(extrapolate_end(&[0, 3, 6, 9, 12, 15]), 18);
+    assert_eq!(extrapolate_end(&[1, 3, 6, 10, 15, 21]), 28);
+    assert_eq!(extrapolate_end(&[10, 13, 16, 21, 30, 45]), 68);
 }
