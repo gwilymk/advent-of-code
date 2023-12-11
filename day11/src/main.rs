09c582093966c78696f4a27f933854d3c2c372d4
Gwilym Inzani <email@gwilym.dev>
2023-12-11 08:00:39 +0000

    Part 2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -3,11 +3,17 @@ use std::fmt::{self, Debug, Formatter};
 fn main() {
     let input = include_str!("../input.txt");
     let mut star_map = StarMap::parse(input);
-    star_map.expand();
+    let mut star_map2 = star_map.clone();
+
+    star_map.expand(2);
 
     println!("Part 1: {}", star_map.sum_of_distances());
+
+    star_map2.expand(1_000_000);
+    println!("Part 2: {}", star_map2.sum_of_distances());
 }
 
+#[derive(Clone)]
 struct StarMap {
     content: Vec<Vec<StarMapEntry>>,
 }
@@ -19,7 +25,7 @@ impl StarMap {
             .map(|line| {
                 line.bytes()
                     .map(|byte| match byte {
-                        b'.' => StarMapEntry::Empty,
+                        b'.' => StarMapEntry::Empty(1),
                         b'#' => StarMapEntry::Galaxy,
                         _ => panic!("Unknown character {byte}"),
                     })
@@ -30,18 +36,18 @@ impl StarMap {
         Self { content }
     }
 
-    pub fn expand(&mut self) {
+    pub fn expand(&mut self, amount: usize) {
         let empty_columns: Vec<_> = (0..self.content[0].len())
             .filter(|&column| {
                 self.content
                     .iter()
-                    .all(|row| matches!(row[column], StarMapEntry::Empty))
+                    .all(|row| matches!(row[column], StarMapEntry::Empty(_)))
             })
             .collect();
 
-        for &empty_column in empty_columns.iter().rev() {
+        for empty_column in empty_columns {
             for row in &mut self.content {
-                row.insert(empty_column, StarMapEntry::Empty);
+                row[empty_column] = StarMapEntry::Empty(amount);
             }
         }
 
@@ -49,14 +55,14 @@ impl StarMap {
             .filter(|&row| {
                 self.content[row]
                     .iter()
-                    .all(|entry| matches!(entry, StarMapEntry::Empty))
+                    .all(|entry| matches!(entry, StarMapEntry::Empty(_)))
             })
             .collect();
 
-        let width = self.content[0].len();
-        for &empty_row in empty_rows.iter().rev() {
-            self.content
-                .insert(empty_row, vec![StarMapEntry::Empty; width]);
+        for empty_row in empty_rows {
+            for item in &mut self.content[empty_row] {
+                *item = StarMapEntry::Empty(amount);
+            }
         }
     }
 
@@ -74,8 +80,23 @@ impl StarMap {
         let mut total = 0;
         for (index, galaxy_position1) in galaxy_positions.iter().enumerate() {
             for galaxy_position2 in galaxy_positions.iter().skip(index + 1) {
-                let distance = galaxy_position2.0.abs_diff(galaxy_position1.0)
-                    + galaxy_position2.1.abs_diff(galaxy_position1.1);
+                let x_min = galaxy_position1.0.min(galaxy_position2.0);
+                let x_max = galaxy_position1.0.max(galaxy_position2.0);
+
+                let y_min = galaxy_position1.1.min(galaxy_position2.1);
+                let y_max = galaxy_position1.1.max(galaxy_position2.1);
+
+                let y_distance = self.content[y_min..y_max]
+                    .iter()
+                    .map(|item| item[x_min].distance())
+                    .sum::<usize>();
+
+                let x_distance = self.content[y_max][x_min..x_max]
+                    .iter()
+                    .map(|item| item.distance())
+                    .sum::<usize>();
+
+                let distance = x_distance + y_distance;
                 total += distance;
             }
         }
@@ -89,7 +110,7 @@ impl Debug for StarMap {
         for row in &self.content {
             for entry in row {
                 match entry {
-                    StarMapEntry::Empty => write!(f, ".")?,
+                    StarMapEntry::Empty(distance) => write!(f, "{distance}")?,
                     StarMapEntry::Galaxy => write!(f, "#")?,
                 }
             }
@@ -101,11 +122,19 @@ impl Debug for StarMap {
     }
 }
 
-#[derive(Clone, Copy, Default)]
+#[derive(Clone, Copy)]
 enum StarMapEntry {
-    #[default]
-    Empty,
+    Empty(usize),
     Galaxy,
+}
+
+impl StarMapEntry {
+    pub fn distance(&self) -> usize {
+        match self {
+            StarMapEntry::Empty(space) => *space,
+            StarMapEntry::Galaxy => 1,
+        }
+    }
 }
 
 #[test]
@@ -122,7 +151,26 @@ fn given_case() {
 #...#.....";
 
     let mut star_map = StarMap::parse(input);
-    star_map.expand();
+    star_map.expand(2);
 
     assert_eq!(star_map.sum_of_distances(), 374);
+}
+
+#[test]
+fn given_case_part2() {
+    let input = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
+
+    let mut star_map: StarMap = StarMap::parse(input);
+    star_map.expand(10);
+
+    assert_eq!(star_map.sum_of_distances(), 1030);
 }
