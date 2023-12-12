f9f96c14b2e22f3c92797048e43f58176aa5bcaf
Gwilym Inzani <email@gwilym.dev>
2023-12-12 08:20:40 +0000

    Part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,15 +1,23 @@
-use std::fmt;
+use std::{collections::HashMap, fmt};
 
 fn main() {
     let input = include_str!("../input.txt");
     println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
 }
 
 fn part1(input: &str) -> usize {
     input.lines().map(arrangements_for_line).sum::<usize>()
 }
 
-#[derive(Clone, Copy, PartialEq, Eq)]
+fn part2(input: &str) -> usize {
+    input
+        .lines()
+        .map(arrangements_for_longer_line)
+        .sum::<usize>()
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 enum SpringInformation {
     Operational,
     Damaged,
@@ -26,10 +34,15 @@ impl fmt::Debug for SpringInformation {
     }
 }
 
-fn working_arrangements(
-    spring_information: &[SpringInformation],
-    contiguous_damaged: &[usize],
+fn working_arrangements<'a>(
+    spring_information: &'a [SpringInformation],
+    contiguous_damaged: &'a [usize],
+    arrangement_cache: &mut HashMap<(&'a [SpringInformation], &'a [usize]), usize>,
 ) -> usize {
+    if let Some(&arrangements) = arrangement_cache.get(&(spring_information, contiguous_damaged)) {
+        return arrangements;
+    }
+
     if contiguous_damaged.is_empty() {
         return if spring_information.contains(&SpringInformation::Damaged) {
             0
@@ -50,16 +63,12 @@ fn working_arrangements(
 
     // try to fit the first value into the spring information from start_position
     while start_position <= spring_information.len() - number_damaged {
-        println!(
-            "{number_damaged} {:?}",
-            &spring_information[start_position..]
-        );
         if spring_information[start_position..(start_position + number_damaged)]
             .contains(&SpringInformation::Operational)
         {
             if spring_information[start_position] == SpringInformation::Damaged {
                 // this one was forced
-                return total_arragements;
+                break;
             }
 
             start_position += 1;
@@ -71,7 +80,7 @@ fn working_arrangements(
         {
             // we have more than number_damaged in a row if the first one here was forced to be damage from the start
             if spring_information[start_position] == SpringInformation::Damaged {
-                return total_arragements;
+                break;
             } else {
                 start_position += 1;
             }
@@ -83,17 +92,22 @@ fn working_arrangements(
                     &spring_information[start_position + number_damaged + 1..]
                 };
 
-            total_arragements +=
-                working_arrangements(remaining_information, &contiguous_damaged[1..]);
+            total_arragements += working_arrangements(
+                remaining_information,
+                &contiguous_damaged[1..],
+                arrangement_cache,
+            );
 
             // if the first value was forced to be damaged, then we can't continue searching
             if spring_information[start_position] == SpringInformation::Damaged {
-                return total_arragements;
+                break;
             }
 
             start_position += 1;
         }
     }
+
+    arrangement_cache.insert((spring_information, contiguous_damaged), total_arragements);
 
     total_arragements
 }
@@ -118,8 +132,22 @@ fn parse(input: &str) -> (Vec<SpringInformation>, Vec<usize>) {
 
 fn arrangements_for_line(input: &str) -> usize {
     let (map, condition_records) = parse(input);
+    let mut cache = HashMap::new();
 
-    working_arrangements(&map, &condition_records)
+    working_arrangements(&map, &condition_records, &mut cache)
+}
+
+fn arrangements_for_longer_line(input: &str) -> usize {
+    let (mut map, condition_records) = parse(input);
+
+    map.push(SpringInformation::MissingInformation);
+    let mut map = map.repeat(5);
+    map.pop();
+    let condition_records = condition_records.repeat(5);
+
+    let mut cache = HashMap::new();
+
+    working_arrangements(&map, &condition_records, &mut cache)
 }
 
 #[test]
@@ -133,4 +161,17 @@ fn check_given_input() {
     assert_eq!(arrangements_for_line("??????#??? 7,1"), 3);
     assert_eq!(arrangements_for_line("?#####???????#.. 5,1,1,1"), 6);
     assert_eq!(arrangements_for_line("???#??#.##. 2,1,2"), 2);
+}
+
+#[test]
+fn check_longer_arrangements() {
+    assert_eq!(arrangements_for_longer_line("???.### 1,1,3"), 1);
+    assert_eq!(arrangements_for_longer_line(".??..??...?##. 1,1,3"), 16384);
+    assert_eq!(arrangements_for_longer_line("?#?#?#?#?#?#?#? 1,3,1,6"), 1);
+    assert_eq!(arrangements_for_longer_line("????.#...#... 4,1,1"), 16);
+    assert_eq!(
+        arrangements_for_longer_line("????.######..#####. 1,6,5"),
+        2500
+    );
+    assert_eq!(arrangements_for_longer_line("?###???????? 3,2,1"), 506250);
 }
