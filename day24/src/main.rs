bb3bb0d26e87dee55ae55f6d1b62883ec21c7375
Gwilym Inzani <email@gwilym.dev>
2023-12-24 17:43:16 +0000

    Lol, the jankiness

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,4 +1,8 @@
-use std::ops::{Add, Mul};
+use itertools::Itertools;
+use std::{
+    collections::HashSet,
+    ops::{Add, Mul, Sub},
+};
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -6,8 +10,11 @@ fn main() {
         "Part1: {}",
         part1(input, 200000000000000., 400000000000000.)
     );
+
+    println!("Part 2: {}", part2(input));
 }
 
+#[derive(Clone)]
 struct Line {
     start_point: Point3d,
     velocity: Point3d,
@@ -65,6 +72,18 @@ impl Add<Point3d> for Point3d {
     }
 }
 
+impl Sub<Point3d> for Point3d {
+    type Output = Point3d;
+
+    fn sub(self, rhs: Point3d) -> Self::Output {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
 impl Mul<f64> for Point3d {
     type Output = Point3d;
 
@@ -112,6 +131,23 @@ impl Line {
 
         Some((t1, t2))
     }
+
+    fn intersect_3d(&self, other: &Line) -> Option<(f64, f64)> {
+        let (t1, t2) = self.intersect_2d(other)?;
+
+        if self.at(t1).distance_sq(&other.at(t2)) < 0.00001 {
+            return None;
+        }
+
+        Some((t1, t2))
+    }
+
+    fn project(&self) -> Self {
+        Self {
+            start_point: self.start_point.project(),
+            velocity: self.velocity.project(),
+        }
+    }
 }
 
 fn part1(input: &str, start: f64, end: f64) -> usize {
@@ -143,6 +179,100 @@ fn part1(input: &str, start: f64, end: f64) -> usize {
     }
 
     collisions
+}
+
+fn part2(input: &str) -> i64 {
+    let mut lines = input
+        .lines()
+        .map(|line| Line::parse(line))
+        .collect::<Vec<_>>();
+
+    let x_velocity = get_velocity(&mut lines, |point| point.x);
+    let y_velocity = get_velocity(&mut lines, |point| point.y);
+    let z_velocity = get_velocity(&mut lines, |point| point.z);
+
+    let rock_velocity = Point3d {
+        x: x_velocity as f64,
+        y: y_velocity as f64,
+        z: z_velocity as f64,
+    };
+
+    println!("{x_velocity} {y_velocity} {z_velocity}");
+
+    let line1 = Line {
+        start_point: lines[0].start_point,
+        velocity: lines[0].velocity - rock_velocity,
+    };
+    let line2 = Line {
+        start_point: lines[1].start_point,
+        velocity: lines[1].velocity - rock_velocity,
+    };
+
+    let (t1, _) = line1.intersect_3d(&line2).expect("does not intersect...");
+    let rock = line1.at(t1);
+
+    println!("rock: {rock:?} with velocity: {rock_velocity:?}");
+    rock.x as i64 + rock.y as i64 + rock.z as i64
+}
+
+fn get_velocity(lines: &mut Vec<Line>, extract: impl Fn(Point3d) -> f64) -> i64 {
+    let mut possible_velocities = HashSet::new();
+    // find all the ones with the same x velocity
+    lines.sort_by_key(|line| extract(line.velocity) as i64);
+    let identical_velocity = lines.iter().group_by(|line| extract(line.velocity));
+    for (velocity, group) in &identical_velocity {
+        let items = group.collect::<Vec<_>>();
+        if items.len() == 1 {
+            continue;
+        }
+
+        let required_for_factors =
+            (extract(items[0].start_point) - extract(items[1].start_point)).abs();
+        let factors_for_velocity = factors(required_for_factors as u64);
+
+        let possible_values_here = factors_for_velocity.iter().flat_map(|&factor| {
+            [
+                velocity as i64 + factor as i64,
+                velocity as i64 - factor as i64,
+            ]
+        });
+
+        if possible_velocities.is_empty() {
+            for possible_value in possible_values_here {
+                possible_velocities.insert(possible_value);
+            }
+        } else {
+            let new_possible_values = possible_values_here.collect::<HashSet<_>>();
+            possible_velocities = possible_velocities
+                .intersection(&new_possible_values)
+                .copied()
+                .collect::<HashSet<_>>();
+        }
+
+        if possible_velocities.len() == 1 {
+            break;
+        }
+    }
+
+    if possible_velocities.len() != 1 {
+        panic!("Could not determine velocity");
+    }
+
+    *possible_velocities.iter().next().unwrap()
+}
+
+fn factors(input: u64) -> Vec<u64> {
+    let mut test = 2;
+    let mut result = vec![1, input];
+
+    while test * test <= input {
+        if input % test == 0 {
+            result.push(test);
+        }
+        test += 1;
+    }
+
+    result
 }
 
 #[test]
@@ -197,5 +327,19 @@ fn part1_given_input() {
             27.,
         ),
         2
+    );
+}
+
+#[test]
+fn part2_given_input() {
+    assert_eq!(
+        part2(
+            "19, 13, 30 @ -2,  1, -2
+18, 19, 22 @ -1, -1, -2
+20, 25, 34 @ -2, -2, -4
+12, 31, 28 @ -1, -2, -1
+20, 19, 15 @  1, -5, -3",
+        ),
+        47
     );
 }
