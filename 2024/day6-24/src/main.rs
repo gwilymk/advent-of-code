58c8187b308de34b79c9a92925988ed01c79fc08
Gwilym Inzani <email@gwilym.dev>
2024-12-06 07:53:33 +0000

    Day 5 part 2

--- 2024/day6-24/src/main.rs
+++ 2024/day6-24/src/main.rs
@@ -2,7 +2,8 @@ use std::collections::HashSet;
 
 fn main() {
     let input = Map::parse(include_str!("./input.txt"));
-    println!("Part 1: {}", positions(&input));
+    println!("Part 1: {}", positions(&input).len());
+    println!("Part 2: {}", additional_obstruction_locations(&input));
 }
 
 #[derive(Clone, Copy, Hash, Eq, PartialEq, Debug)]
@@ -100,9 +101,39 @@ impl Map {
 
         self.obstructions[point.y as usize][point.x as usize]
     }
+
+    fn add_obstruction(&mut self, point: Point) {
+        self.obstructions[point.y as usize][point.x as usize] = true;
+    }
 }
 
-fn positions(input: &Map) -> usize {
+fn does_route_loop(input: &Map) -> bool {
+    let mut current_point = input.position;
+    let mut current_direction = input.direction;
+
+    let mut visited_points = HashSet::new();
+
+    while input.contains_point(current_point) {
+        if !visited_points.insert((current_point, current_direction)) {
+            return true;
+        }
+
+        loop {
+            let potential_new_point = current_point.in_direction(current_direction);
+
+            if !input.is_obstructed(potential_new_point) {
+                current_point = potential_new_point;
+                break;
+            }
+
+            current_direction = current_direction.rotate_right();
+        }
+    }
+
+    false
+}
+
+fn positions(input: &Map) -> HashSet<Point> {
     let mut current_point = input.position;
     let mut current_direction = input.direction;
 
@@ -123,7 +154,21 @@ fn positions(input: &Map) -> usize {
         }
     }
 
-    visited_points.len()
+    visited_points
+}
+
+fn additional_obstruction_locations(input: &Map) -> usize {
+    let positions_to_search = positions(input);
+
+    positions_to_search
+        .iter()
+        .filter(|point| {
+            let mut input = input.clone();
+            input.add_obstruction(**point);
+
+            does_route_loop(&input)
+        })
+        .count()
 }
 
 #[test]
@@ -141,5 +186,7 @@ fn given_input() {
 ......#...",
     );
 
-    assert_eq!(positions(&input), 41);
+    assert_eq!(positions(&input).len(), 41);
+
+    assert_eq!(additional_obstruction_locations(&input), 6);
 }
