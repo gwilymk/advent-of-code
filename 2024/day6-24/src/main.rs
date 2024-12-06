4977450f2923095167d49729ad46fa74e4d0c39b
Gwilym Inzani <email@gwilym.dev>
2024-12-06 07:44:43 +0000

    Day 6 part 1

---
+++ 2024/day6-24/src/main.rs
@@ -0,0 +1,145 @@
+use std::collections::HashSet;
+
+fn main() {
+    let input = Map::parse(include_str!("./input.txt"));
+    println!("Part 1: {}", positions(&input));
+}
+
+#[derive(Clone, Copy, Hash, Eq, PartialEq, Debug)]
+struct Point {
+    x: isize,
+    y: isize,
+}
+
+impl Point {
+    fn in_direction(self, direction: Direction) -> Self {
+        match direction {
+            Direction::Up => Self {
+                x: self.x,
+                y: self.y - 1,
+            },
+            Direction::Right => Self {
+                x: self.x + 1,
+                y: self.y,
+            },
+            Direction::Down => Self {
+                x: self.x,
+                y: self.y + 1,
+            },
+            Direction::Left => Self {
+                x: self.x - 1,
+                y: self.y,
+            },
+        }
+    }
+}
+
+#[derive(Clone, Copy, Hash, Eq, PartialEq, Debug)]
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+impl Direction {
+    fn rotate_right(self: Direction) -> Self {
+        match self {
+            Direction::Up => Direction::Right,
+            Direction::Right => Direction::Down,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+        }
+    }
+}
+
+#[derive(Clone)]
+struct Map {
+    obstructions: Vec<Vec<bool>>,
+    position: Point,
+    direction: Direction,
+}
+
+impl Map {
+    fn parse(input: &str) -> Self {
+        let obstructions = input
+            .split('\n')
+            .map(|line| line.chars().map(|c| c == '#').collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+
+        let start_point = input.find('^').unwrap();
+        let start_point_x = start_point % (obstructions[0].len() + 1);
+        let start_point_y = start_point / (obstructions[0].len() + 1);
+
+        Self {
+            obstructions,
+            position: Point {
+                x: start_point_x as isize,
+                y: start_point_y as isize,
+            },
+            direction: Direction::Up,
+        }
+    }
+
+    fn contains_point(&self, point: Point) -> bool {
+        if point.x < 0 || point.x >= self.obstructions[0].len() as isize {
+            return false;
+        }
+
+        if point.y < 0 || point.y >= self.obstructions.len() as isize {
+            return false;
+        }
+
+        true
+    }
+
+    fn is_obstructed(&self, point: Point) -> bool {
+        if !self.contains_point(point) {
+            return false;
+        }
+
+        self.obstructions[point.y as usize][point.x as usize]
+    }
+}
+
+fn positions(input: &Map) -> usize {
+    let mut current_point = input.position;
+    let mut current_direction = input.direction;
+
+    let mut visited_points = HashSet::new();
+
+    while input.contains_point(current_point) {
+        visited_points.insert(current_point);
+
+        loop {
+            let potential_new_point = current_point.in_direction(current_direction);
+
+            if !input.is_obstructed(potential_new_point) {
+                current_point = potential_new_point;
+                break;
+            }
+
+            current_direction = current_direction.rotate_right();
+        }
+    }
+
+    visited_points.len()
+}
+
+#[test]
+fn given_input() {
+    let input = Map::parse(
+        "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...",
+    );
+
+    assert_eq!(positions(&input), 41);
+}
