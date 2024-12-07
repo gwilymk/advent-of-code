ed969e168f00cfce1f4dd6cd71a4528379e19de6
Gwilym Inzani <email@gwilym.dev>
2024-12-07 08:20:11 +0000

    Part 2

--- 2024/day7-24/src/main.rs
+++ 2024/day7-24/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("Part 1: {}", num_solvable(include_str!("input.txt"), false));
+    println!("Part 2: {}", num_solvable(include_str!("input.txt"), true));
 }
 
 fn recursive_solvable_equation(expected_result: i64, inputs: &[i64], include_concat: bool) -> bool {
@@ -24,6 +25,26 @@ fn recursive_solvable_equation(expected_result: i64, inputs: &[i64], include_con
         include_concat,
     ) {
         return true;
+    }
+
+    if include_concat && expected_result > 0 {
+        let expected_result_as_str = expected_result.to_string();
+        let concat_amount = test_value.to_string();
+
+        if expected_result_as_str.ends_with(&concat_amount) {
+            let without_concat =
+                &expected_result_as_str[..expected_result_as_str.len() - concat_amount.len()];
+
+            if !without_concat.is_empty()
+                && recursive_solvable_equation(
+                    without_concat.parse().unwrap(),
+                    &inputs[..inputs.len() - 1],
+                    include_concat,
+                )
+            {
+                return true;
+            }
+        }
     }
 
     false
@@ -65,5 +86,21 @@ fn given_input() {
             false
         ),
         3749
+    );
+
+    assert_eq!(
+        num_solvable(
+            "190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20",
+            true
+        ),
+        11387
     );
 }
