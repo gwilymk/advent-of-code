1c9dbaf69b6b0be36f1512e82992c927faada58f
Gwilym Inzani <email@gwilym.dev>
2024-12-11 16:24:31 +0000

    More utils

--- 2024/utils/src/lib.rs
+++ 2024/utils/src/lib.rs
@@ -38,12 +38,71 @@ impl<T> Grid2<T> {
         self.points.get(y)?.get(x)
     }
 
-    pub fn neighbours<V: TryInto<usize> + agb_fixnum::FixedWidthUnsignedInteger>(
+    pub fn neighbours_with_points<V: TryInto<i32> + agb_fixnum::FixedWidthUnsignedInteger>(
+        &self,
+        point: impl Into<Vector2D<V>>,
+        include_diagonals: bool,
+    ) -> impl Iterator<Item = (&T, Vector2D<i32>)> {
+        let point = point.into();
+
+        (-1..=1).flat_map(move |y| {
+            (-1..=1).filter_map(move |x| {
+                if !include_diagonals && x * y != 0 {
+                    return None;
+                }
+
+                if x == 0 && y == 0 {
+                    return None;
+                }
+
+                let cx = point.x.try_into();
+                let cy = point.y.try_into();
+
+                match (cx, cy) {
+                    (Ok(cx), Ok(cy)) => {
+                        let px = cx + x;
+                        let py = cy + y;
+
+                        Some((self.get::<i32>((px, py))?, Vector2D::new(px, py)))
+                    }
+                    _ => None,
+                }
+            })
+        })
+    }
+
+    pub fn neighbours<V: TryInto<i32> + agb_fixnum::FixedWidthUnsignedInteger>(
         &self,
         point: impl Into<Vector2D<V>>,
         include_diagonals: bool,
     ) -> impl Iterator<Item = &T> {
-        let point = point.into();
+        self.neighbours_with_points(point, include_diagonals)
+            .map(|(n, _)| n)
+    }
+}
+
+pub trait AllPairsExt<Item> {
+    fn all_pairs<'a>(&'a self) -> impl Iterator<Item = (Item, Item)> + 'a
+    where
+        Item: 'a;
+}
+
+impl<T, U> AllPairsExt<T> for U
+where
+    U: AsRef<[T]>,
+    T: Clone + 'static,
+{
+    fn all_pairs<'a>(&'a self) -> impl Iterator<Item = (T, T)> + 'a
+    where
+        T: 'a,
+    {
+        let slice: &[T] = self.as_ref();
+        slice.iter().enumerate().flat_map(move |(i, x)| {
+            slice
+                .iter()
+                .skip(i + 1)
+                .map(move |y| (x.clone(), y.clone()))
+        })
     }
 }
 
@@ -65,5 +124,33 @@ mod tests {
         let grid = Grid2::parse("123\n456\n789", |line| {
             line.chars().map(|c| c.to_digit(10).unwrap()).collect()
         });
+
+        let non_diagonal = grid.neighbours::<i32>((0, 0), false).collect::<Vec<_>>();
+        assert_eq!(non_diagonal, &[&2, &4]);
+
+        let diagonal = grid.neighbours::<i32>((0, 0), true).collect::<Vec<_>>();
+        assert_eq!(diagonal, &[&2, &4, &5]);
+    }
+
+    #[test]
+    fn all_pairs() {
+        let test_input = &[1, 2, 3, 4, 5];
+        let pairs = test_input.all_pairs().collect::<Vec<_>>();
+
+        assert_eq!(
+            pairs,
+            &[
+                (1, 2),
+                (1, 3),
+                (1, 4),
+                (1, 5),
+                (2, 3),
+                (2, 4),
+                (2, 5),
+                (3, 4),
+                (3, 5),
+                (4, 5)
+            ]
+        )
     }
 }
