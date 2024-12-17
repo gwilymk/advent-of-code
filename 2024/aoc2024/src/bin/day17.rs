3f9f026172add6c3a73ffb527ce54fa1a708fefc
Gwilym Inzani <email@gwilym.dev>
2024-12-17 09:19:28 +0000

    Part 2

--- 2024/aoc2024/src/bin/day17.rs
+++ 2024/aoc2024/src/bin/day17.rs
@@ -1,4 +1,5 @@
 use aoc2024::get_input;
+use itertools::Itertools;
 use regex::Regex;
 use std::fmt::Write;
 use std::sync::LazyLock;
@@ -7,6 +8,27 @@ fn main() {
     let input = get_input(17);
     let mut machine = Machine::parse(&input);
     println!("Part 1: {}", machine.until_halt());
+
+    let mut machine2 = Machine::parse(&input);
+    let a = machine2
+        .make_quine(&machine.program, 0)
+        .expect("Expect this to work");
+    println!("Part 2: {a}");
+
+    machine2.a = a;
+    machine2.b = 0;
+    machine2.c = 0;
+    machine2.pc = 0;
+
+    let output = machine2
+        .until_halt()
+        .split(',')
+        .map(|i| i.parse().unwrap())
+        .collect::<Vec<usize>>();
+
+    assert_eq!(output, machine2.program);
+
+    machine2.disassemble();
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, enumn::N)]
@@ -69,6 +91,61 @@ impl Machine {
         }
     }
 
+    fn disassemble(&self) {
+        for (instr, op) in self.program.iter().tuples() {
+            Self::disassemble_one(*instr, *op);
+        }
+    }
+
+    fn disassemble_one(instr: usize, op: usize) {
+        let instr = Instruction::n(instr as i64).unwrap();
+
+        match instr {
+            Instruction::Adv
+            | Instruction::Bst
+            | Instruction::Bdv
+            | Instruction::Cdv
+            | Instruction::Out => {
+                println!("{instr:?} {:?}", Operand::from_int(op));
+            }
+            Instruction::Bxc => {
+                println!("Bxc");
+            }
+            _ => {
+                println!("{instr:?} {op}");
+            }
+        }
+    }
+
+    fn until_out(&mut self) -> Option<usize> {
+        while self.pc < self.program.len() {
+            let instr = Instruction::n(self.program[self.pc] as i64).unwrap();
+            let op = self.program[self.pc + 1];
+
+            match (instr, op) {
+                (Instruction::Adv, op) => self.a >>= self.get_op(op),
+                (Instruction::Blx, op) => self.b ^= op,
+                (Instruction::Bst, op) => self.b = self.get_op(op) % 8,
+                (Instruction::Jnz, op) => {
+                    if self.a != 0 {
+                        self.pc = op;
+                        continue;
+                    }
+                }
+                (Instruction::Bxc, _) => self.b ^= self.c,
+                (Instruction::Out, op) => {
+                    return Some(self.get_op(op) % 8);
+                }
+                (Instruction::Bdv, op) => self.b = self.a >> self.get_op(op),
+                (Instruction::Cdv, op) => self.c = self.a >> self.get_op(op),
+            }
+
+            self.pc += 2;
+        }
+
+        None
+    }
+
     fn until_halt(&mut self) -> String {
         let mut output = String::new();
 
@@ -110,6 +187,58 @@ impl Machine {
             Operand::B => self.b,
             Operand::C => self.c,
         }
+    }
+
+    fn make_quine(&mut self, remaining_output: &[usize], current_a: usize) -> Option<usize> {
+        // assumptions:
+        // * jnz 0 is the last instruction
+        // * b and c start at 0
+        // * a is processed 3 bits at a time
+
+        if remaining_output.is_empty() {
+            return Some(current_a);
+        }
+
+        let target_value = remaining_output[remaining_output.len() - 1];
+        let remaining = &remaining_output[..remaining_output.len() - 1];
+
+        for potential_a in 0..8 {
+            self.a = current_a << 3 | potential_a;
+            self.b = 0;
+            self.c = 0;
+            self.pc = 0;
+
+            if Some(target_value) == self.until_out() {
+                let next_a = (current_a << 3) | potential_a;
+
+                if let Some(quine) = self.make_quine(remaining, next_a) {
+                    return Some(quine);
+                }
+            }
+        }
+
+        None
+
+        // let mut remaining_output = self.program.clone();
+        // let mut current_a = 0;
+
+        // 'outer: while let Some(output) = remaining_output.pop() {
+        //     for potential_a in 0..8 {
+        //         self.a = current_a << 3 | potential_a;
+        //         self.b = 0;
+        //         self.c = 0;
+        //         self.pc = 0;
+
+        //         if Some(output) == self.until_out() {
+        //             current_a = (current_a << 3) | potential_a;
+        //             continue 'outer;
+        //         }
+        //     }
+
+        //     println!("Couldn't find for {output} with remaining {remaining_output:?}");
+        // }
+
+        // current_a
     }
 }
 
