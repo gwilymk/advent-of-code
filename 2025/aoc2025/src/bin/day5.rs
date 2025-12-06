53dd1f5ce7053aa78daadbc64e45c0832e8254e4
Gwilym Inzani <email@gwilym.dev>
2025-12-06 18:16:31 +0000

    Day 5 part 2

--- 2025/aoc2025/src/bin/day5.rs
+++ 2025/aoc2025/src/bin/day5.rs
@@ -5,6 +5,7 @@ fn main() {
     let (fresh_ranges, ingredients) = parse(&input);
 
     println!("Part 1: {}", part1(&fresh_ranges, &ingredients));
+    println!("Part 2: {}", part2(&fresh_ranges));
 }
 
 fn part1(fresh_ranges: &[RangeInclusive<u64>], ingredients: &[u64]) -> usize {
@@ -12,6 +13,35 @@ fn part1(fresh_ranges: &[RangeInclusive<u64>], ingredients: &[u64]) -> usize {
         .iter()
         .filter(|ingredient| fresh_ranges.iter().any(|r| r.contains(ingredient)))
         .count()
+}
+
+fn part2(fresh_ranges: &[RangeInclusive<u64>]) -> u64 {
+    let mut fresh_ranges = Vec::from(fresh_ranges);
+    fresh_ranges.sort_unstable_by_key(|r| *r.start());
+
+    // we want to deduplicate the ranges and combine overlapping ones
+    // with the ranges sorted by start point, we can consider them in pairs
+    let mut current = 0;
+    while current < fresh_ranges.len() - 1 {
+        let first = &fresh_ranges[current];
+        let second = &fresh_ranges[current + 1];
+
+        if first.end() >= second.start() {
+            // the two ranges overlap
+            let combined = *first.start()..=*second.end().max(first.end());
+
+            fresh_ranges[current] = combined;
+            fresh_ranges.remove(current + 1);
+        } else {
+            current += 1;
+        }
+    }
+
+    // now we can count the number of elements in each range
+    fresh_ranges
+        .iter()
+        .map(|range| *range.end() - *range.start() + 1)
+        .sum()
 }
 
 fn parse(input: &str) -> (Vec<RangeInclusive<u64>>, Vec<u64>) {
@@ -31,4 +61,28 @@ fn parse(input: &str) -> (Vec<RangeInclusive<u64>>, Vec<u64>) {
         .collect();
 
     (fresh_ranges, ingredients)
+}
+
+#[cfg(test)]
+const TEST_INPUT: &str = "3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32";
+
+#[test]
+fn test_part2() {
+    let (ranges, _) = parse(TEST_INPUT);
+    assert_eq!(part2(&ranges), 14);
+}
+
+#[test]
+fn test_part2_manual_cases() {
+    assert_eq!(part2(&[10..=10, 11..=13, 10..=10, 13..=14]), 5);
 }
